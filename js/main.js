2bc63ba6fc1c4e84c1584f1fec1349e9bedbf7a7
Peeyush Kushwaha <peeyush.p97@gmail.com>
2018-03-21 16:29:55 +0530

    Stage-1: Implement basic word lookup
    Todo: snapshotting the last-searched-for word

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-alert("Hello, World")
-
 fetch("data/dictionary.json")
   .then(function (response) {
     // no var qualifier means dictionary is a global variable
     response.json().then((jsonObject) => dictionary = jsonObject)
-    alert("diction successsful")
   })
   .catch(function (error) {
     alert("oh noes, an error while fetching dictionary")
   })
 
-new Vue({
+var vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    searchString: '',
+    definition: '',
+    screen: 'welcome'
+  },
+  methods: {
+    lookupDictionary() {
+      var word = this.searchString.toUpperCase()
+      console.log(word, dictionary, dictionary[word])
+      if (dictionary[word] !== undefined) {
+        this.definition = dictionary[word]
+        this.screen = 'word'
+      } else {
+        this.screen = 'error'
+      }
+    }
   }
 })
