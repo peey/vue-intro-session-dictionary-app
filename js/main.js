1fed223153013c2804ddeeb088755a0690df64e7
Peeyush Kushwaha <peeyush.p97@gmail.com>
2018-03-21 18:14:29 +0530

    Stage-2: Basic suggestions

--- js/main.js
+++ js/main.js
@@ -7,12 +7,31 @@ fetch("data/dictionary.json")
     alert("oh noes, an error while fetching dictionary")
   })
 
+
+function dictionaryWordSuggestions(partial, howMany = 4) {
+  var words = Object.keys(dictionary)
+  partial = partial.toUpperCase()
+  var result = []
+  for (let i = 0; i < words.length; i++) {
+    if (new RegExp("^" + partial).test(words[i])) {
+      result.push(words[i])
+    }    
+
+    if (result.length >= howMany) {
+      break
+    }
+  }
+
+  return result
+}
+
 var vm = new Vue({
   el: '#app',
   data: {
     searchString: '',
     lastSearchString: '',
     definition: '',
+    searchSuggestions: [],
     screen: 'welcome'
   },
   methods: {
@@ -26,6 +45,11 @@ var vm = new Vue({
       } else {
         this.screen = 'error'
       }
+    },
+
+    getSearchSuggestions() {
+      this.screen = 'searching'
+      this.searchSuggestions = dictionaryWordSuggestions(this.searchString)
     }
-  }
+  },
 })
